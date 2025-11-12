729ddddd4ba045548ee8f72057f644bc0b4dad80
RiskyMH <git@riskymh.dev>
2025-11-13 00:18:52 +1100

    nicer

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -25,6 +25,12 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     Frame: (props) => <div {...props} className={cn("p-2 border bg-fd-card shadow-md rounded-2xl *:my-0! [&_p_>_*]:my-0! [&_p_>_.rounded-lg]:rounded-xl my-[2em]", props.className)}>{props.children}</div>,
     Badge: (props) => <span className={cn("bg-fd-accent text-fd-accent-foreground rounded-md px-2 py-1 text-xs", props.className)}>{props.children}</span>,
     Image: (props) => <img {...props} />,
+    Accordion: (props) => (
+      <AccordionComponents.Accordion
+        id={typeof props.title === 'string' ? props.title.toLowerCase().replace(/[^a-z0-9-]/g, '-').replace(/-+/g, '-').replace(/^-+|-+$/g, '') : undefined}
+        {...props}
+      />
+    ),
     ...components,
   };
 }
