09bf8bc87c9a9250f2af3244466abc9ef1f5971b
RiskyMH <git@riskymh.dev>
2025-11-22 09:31:23 +1100

    upd

--- scripts/check-links.ts
+++ scripts/check-links.ts
@@ -84,7 +84,7 @@ function getHeadings({ data }: InferPageType<typeof source>): string[] {
 function getFiles() {
   const promises = source.getPages().map(
     async (page): Promise<FileObject> => ({
-      path: page.absolutePath,
+      path: page.absolutePath || page.path,
       content: await page.data.getText("raw"),
       url: page.url,
       data: page.data,
