d2ecdf29ec8a754b471d434c76e63766a3d5a67e
RiskyMH <git@riskymh.dev>
2026-05-24 05:23:13 +1000

    sync

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,7 +4,7 @@ import * as AccordionComponents from 'fumadocs-ui/components/accordion';
 import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
 import { Step, Steps } from 'fumadocs-ui/components/steps';
 import { ParamField } from './components/ui/param-field';
-import { Tweet } from 'react-tweet'
+// import { Tweet } from 'react-tweet'
 import type { MDXComponents } from 'mdx/types';
 import { cn } from '@/lib/cn';
 
@@ -22,7 +22,7 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
         <Pre>{props.children}</Pre>
       </CodeBlock>
     ),
-    ReactTweet: Tweet,
+    // ReactTweet: Tweet,
     Frame: (props) => <div {...props} className={cn("p-2 border bg-fd-card shadow-md rounded-2xl *:my-0! [&_p_>_*]:my-0! [&_p_>_.rounded-lg]:rounded-xl [&_>_img]:rounded-xl my-[2em]", props.className)}>{props.children}</div>,
     Badge: (props) => <span className={cn("bg-fd-accent text-fd-accent-foreground rounded-md px-2 py-1 text-xs", props.className)}>{props.children}</span>,
     Image: (props) => <img {...props} />,
